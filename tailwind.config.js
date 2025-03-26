3bc4283a302d951b6a5cc2173d82b060986f2d05
Orion <wilman3d@hotmail.com>
2025-03-25 19:17:11 -0500

    Add support for Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class', // o 'media' si prefieres modo oscuro basado en las preferencias del sistema
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
